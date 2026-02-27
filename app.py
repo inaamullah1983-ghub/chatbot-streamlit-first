f36a3f3c77c58c221b622f7d9d2d5284bf034e68
Inaam Ullah <inaamullah1983@gmail.com>
2026-02-27 12:38:50 +0500

    Fixed variable typo and updated Gemini version

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ client = genai.Client(api_key=my_key)
 st.title("Talk to Agent")
 st.write("This app demonstrates a conversational agent using Google Gemini. The agent can answer questions, provide information, and engage in a dialogue with the user.")
 
-uder_input = st.text_input("Ask Your Question Here")
+user_input = st.text_input("Ask Your Question Here")
 if st.button("Submit"):
     with st.spinner("Generating response..."):
         response = client.models.generate_content(
-            model="gemini-2.5-flash", contents=user_input
+            model="gemini-2.0-flash", contents=user_input
         )    
     st.write(response.text)
